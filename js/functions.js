fa0865f238fa337a71a07e2d218b2d8fe22d4b36
Guillermo Isaac Arvizu Juarez <g.arvizu@cheilmex.local>
2021-03-02 09:25:18 -0600

    issue download file

--- js/functions.js
+++ js/functions.js
@@ -12,28 +12,6 @@ function openTab(evt, templateType) {
     evt.currentTarget.className += " active";
 }
 
-function download(filename, text) {
-    var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    element.setAttribute('download', filename);
-
-    element.style.display = 'none';
-    document.body.appendChild(element);
-
-    element.click();
-
-    document.body.removeChild(element);
-}
-
-// Start file download.
-document.getElementById("dwn-btn").addEventListener("click", function() {
-    // Generate download of detailed_banner.txt file with some content
-    var text = document.getElementById("output2").value;
-    var filename = "detailed_banner.txt";
-
-    download(filename, text);
-}, false);
-
 let colorInput = document.querySelector('#color');
 let hexInput = document.querySelector('#hex');
 
